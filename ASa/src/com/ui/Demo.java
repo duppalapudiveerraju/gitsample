6f750cf185da3ad0194dbcef3a3b462ec30c9323
duppalapudiveerraju <duppalapudiveerraju@gmail.com>
2016-12-07 21:28:37 +0530

    Update Demo.java
    new modification

--- ASa/src/com/ui/Demo.java
+++ ASa/src/com/ui/Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("welcome to second");
 	System.out.println("welcome to ag");
 	System.out.println("wewewewew");
+	system.out.println("oy");
 }
 }
